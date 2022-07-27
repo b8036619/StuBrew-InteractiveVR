363fd5ada7b79e290add7fde257c5f359f10ee5d
b8036619 <60885876+b8036619@users.noreply.github.com>
2022-07-27 20:07:15 +0100

    Bucket and Stairs
    Stairs now walkable. Bucket has a pouring script and particle system. Shelf also added

---
+++ Stu Brew Interactive VR/Assets/Scripts/Pour.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pour : MonoBehaviour
+{
+    public int pourThreshold = 45;
+    public Transform origin = null;
+    //public GameObject streamPrefab = null;
+    public ParticleSystem particlePour;
+
+    private bool isPouring = false;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particlePour.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool pourCheck = CalculatePourAngle() < pourThreshold;
+
+        if (isPouring != pourCheck)
+        {
+            isPouring = pourCheck;
+
+            if (isPouring)
+            {
+                StartPour();
+            }
+            else
+            {
+                EndPour();
+            }
+        }
+    }
+
+    private void StartPour()
+    {
+        particlePour.Play();
+    }
+
+    private void EndPour()
+    {
+        particlePour.Stop();
+    }
+
+    private float CalculatePourAngle()
+    {
+        return transform.up.y * Mathf.Rad2Deg;
+    }
+
+}
